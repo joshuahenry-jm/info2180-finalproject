ec7fad2bd5ecaee0c932d707d5a4c17243608a19
britneyhawthorne5-bit <britneyhawthorne5@gmail.com>
2025-12-26 16:53:42 -0500

    Delete get_contacts.php

--- get_contacts.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "../config/database.php";
-
-$sql = "SELECT contacts.*, users.firstname AS assigned_name
-        FROM contacts
-        LEFT JOIN users ON contacts.assigned_to = users.id";
-
-$stmt = $pdo->query($sql);
-echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
